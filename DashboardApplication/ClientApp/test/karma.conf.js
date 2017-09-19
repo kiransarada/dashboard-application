345e992927e89d5ecb16657061eb99fcde2140e6
Trihatmaja, Sukra <sukra.trihatmaja@palador.com>
2017-09-19 17:21:57 +0700

    include missing karma.conf.js

---
+++ DashboardApplication/ClientApp/test/karma.conf.js
@@ -0,0 +1,26 @@
+﻿module.exports = function (config) {
+    config.set({
+        basePath: ".",
+        frameworks: ["jasmine"],
+        files: [
+            "./test.main.ts"
+        ],
+        preprocessors: {
+            "./test.main.ts": ["webpack"]
+        },
+        mime: {
+            "application/javascript": ["ts"]
+        },
+        webpack: require("../../webpack.config.js"),
+        webpackMiddleware: {
+            stats: "errors-only"
+        },
+        reporters: ["progress"],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        singleRun: false,
+        browsers: ["Chrome"],
+        concurrency: Infinity
+    });
+}
