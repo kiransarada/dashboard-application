d163fc89302e34cc988ec059a8098c208e2ce497
Trihatmaja, Sukra <sukra.trihatmaja@palador.com>
2017-10-04 17:46:26 +0700

    add shallow and isolated test

---
+++ DashboardApplication/ClientApp/test/User/user-dashboard.component.spec.ts
@@ -0,0 +1,29 @@
+﻿import { TestBed, ComponentFixture, async } from "@angular/core/testing";
+import { NO_ERRORS_SCHEMA, DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
+import { ModalModule } from "ngx-bootstrap/modal";
+
+import { UserDashboardComponent } from "../../app/User/user-dashboard.component";
+
+describe("UserDashboardComponent (Shallow)", () => {
+    let componentFixture: ComponentFixture<UserDashboardComponent>;
+    let debugElement: DebugElement;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [ModalModule.forRoot()],
+            declarations: [UserDashboardComponent],
+            schemas: [NO_ERRORS_SCHEMA]
+        });
+
+        TestBed.compileComponents();
+    }));
+
+    it("should have 'Users' on panel header text", () => {
+        componentFixture = TestBed.createComponent(UserDashboardComponent);
+        debugElement = componentFixture.debugElement;
+        componentFixture.detectChanges();
+        const panelHeaderElement = debugElement.query(By.css(".panel-heading")).nativeElement;
+        expect(panelHeaderElement.textContent).toContain("Users");
+    });
+});
---
+++ DashboardApplication/ClientApp/test/User/user-form.component.spec.ts
@@ -0,0 +1,19 @@
+﻿import { UserFormComponent, FormType } from "../../app/User/user-form.component";
+
+describe("UserFormComponent (Isolated)", () => {
+    let component: UserFormComponent;
+
+    beforeEach(() => {
+        component = new UserFormComponent(undefined, undefined);
+    });
+
+    it("should get correct title based on form type", () => {
+		// checking for Add form type
+        component.formType = FormType.Add;
+        expect(component.title).toEqual("Add New User");
+
+        // checking for Edit form type
+        component.formType = FormType.Edit;
+        expect(component.title).toEqual("Edit User");
+    });
+});
