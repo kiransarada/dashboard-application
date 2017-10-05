a4940a7d02af238f940fe5a67cdf792fbaa54164
Trihatmaja, Sukra <sukra.trihatmaja@palador.com>
2017-10-05 10:47:53 +0700

    add mock service and integration test file

---
+++ DashboardApplication/ClientApp/test/User/mock-user.service.ts
@@ -0,0 +1,26 @@
+﻿import { Observable } from "rxjs/Observable";
+import { Subject } from "rxjs/Subject";
+
+import { IUser } from "../../app/User/user.interface";
+
+export class MockUserService {
+    private userSubject = new Subject<IUser[]>();
+    $users = this.userSubject.asObservable();
+
+    loadUsers() {
+        const mockUsers = [{ id: 1, name: "John" }] as IUser[];
+        this.userSubject.next(mockUsers);
+    }
+
+    save(user: IUser): Observable<boolean> {
+        return Observable.create();
+    }
+
+    delete(user: IUser): Observable<boolean> {
+        return Observable.create();
+    }
+
+    onComponentDestroy() {
+        this.userSubject.next(undefined);
+    }
+}
---
+++ DashboardApplication/ClientApp/test/User/user.module.spec.ts
@@ -0,0 +1,41 @@
+﻿import { TestBed, ComponentFixture, async } from "@angular/core/testing";
+import { DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
+import { ModalModule } from "ngx-bootstrap/modal";
+
+import { UserModule } from "../../app/User/user.module";
+import { UserDashboardComponent } from "../../app/User/user-dashboard.component";
+import { UserListComponent } from "../../app/User/user-list.component";
+import { UserService } from "../../app/User/user.service";
+import { MockUserService } from "./mock-user.service";
+
+describe("UserModule (Integration)", () => {
+    let componentFixture: ComponentFixture<UserDashboardComponent>;
+    let userDashboardComponent: UserDashboardComponent;
+    let userDashboardDebugElement: DebugElement;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [UserModule, ModalModule.forRoot()]
+        });
+
+        TestBed.overrideProvider(UserService, {
+            useValue: new MockUserService()
+        });
+
+        TestBed.compileComponents();
+    }));
+
+    beforeEach(() => {
+        componentFixture = TestBed.createComponent(UserDashboardComponent);
+        userDashboardComponent = componentFixture.componentInstance;
+        userDashboardDebugElement = componentFixture.debugElement;
+        componentFixture.detectChanges();
+    });
+
+    it("should have 1 user listed on the first load", () => {
+        const userListDebugElement = userDashboardDebugElement.query(By.css("sg-user-list"));
+        const userListComponent = userListDebugElement.componentInstance as UserListComponent;
+        expect(userListComponent.users.length).toEqual(1);
+    });
+});
