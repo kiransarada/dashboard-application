d92c457efbcdec494a54ef6a039a624760d2c94a
Trihatmaja, Sukra <sukra.trihatmaja@palador.com>
2017-10-06 12:44:21 +0700

    add filter pipe

--- DashboardApplication/ClientApp/app/User/user.module.ts
+++ DashboardApplication/ClientApp/app/User/user.module.ts
@@ -6,10 +6,11 @@ import { UserDashboardComponent } from "./user-dashboard.component";
 import { UserFormComponent } from "./user-form.component";
 import { UserListComponent } from "./user-list.component";
 import { UserService } from "./user.service";
+import { FilterPipe } from "../Shared/filter.pipe";
 
 @NgModule({
     imports: [CommonModule, FormsModule],
-    declarations: [UserDashboardComponent, UserFormComponent, UserListComponent],
+    declarations: [UserDashboardComponent, UserFormComponent, UserListComponent, FilterPipe],
     entryComponents: [UserFormComponent],
     exports: [UserDashboardComponent],
     providers: [UserService]
